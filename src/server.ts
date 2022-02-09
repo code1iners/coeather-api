d2ee3a5b12d0d71f764f9204311d4f4cb612286e
Codeliner <codeliner96@gmail.com>
2022-02-09 21:16:38 +0900

    Deploy test.

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,7 @@ import { initEnvironment } from "./utils/envUtils";
       };
     },
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    introspection: true,
   });
 
   await apolloServer.start();
